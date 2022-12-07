071195c6ccda8fb23a2eccebc9b646d46896f7c4
sahilkumarsahoo <email.sahil@yahoo.com>
2022-12-07 20:45:50 +0530

    all done

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+function trimAll(value: any): any {
+    if (!value) return value;
+    if (Array.isArray(value)) return value.map((each) => trimAll(each));
+    if (typeof value == 'object') {
+        const _obj: any = {};
+        for (const key in value) {
+            _obj[key] = trimAll(value[key]);
+        }
+        return _obj;
+    }
+    if (typeof value == 'string') return value.trim();
+    return value;
+}
+
+export default trimAll;
